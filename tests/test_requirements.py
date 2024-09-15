7e7129b46d35cf8437540e41e95ea254d35d0963
cgeils25 <cgeils@email.sc.edu>
2024-09-15 18:33:08 -0400

    test requirements imports

---
+++ tests/test_requirements.py
@@ -0,0 +1,22 @@
+"""
+Tests imports of required packages from requirements.txt:
+
+- jupyter
+- matplotlib
+- numpy
+- pandas
+- pytest
+- torch
+- rdkit
+- sklearn
+- seaborn
+- tqdm
+"""
+
+# man this would be so much easier if the name of a package was the same as the name of the module you import from it
+required_packages = ['jupyter', 'matplotlib', 'numpy', 'pandas', 'pytest', 'torch', 'rdkit', 'sklearn', 'seaborn', 'tqdm']
+
+def test_imports():
+    # test that all required packages can be imported
+    for package in required_packages:
+        assert __import__(package) is not None
