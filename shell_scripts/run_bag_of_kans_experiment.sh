3c1f47dfc4fc66db1aa26a47b168803e80f47dbe
Christian Geils <cgeils@email.sc.edu>
2025-04-08 12:34:27 -0400

    remove shell script for bag of kans experiment

--- shell_scripts/run_bag_of_kans_experiment.sh
+++
@@ -1,9 +0,0 @@
-python full_experiments/run_bag_of_KANs_experiment.py \
-    --data_path datasets/aqueous_solubility_delaney.csv\
-    --n_trials 1 \
-    --variance_threshold 0.1 \
-    --random_seed 1738 \
-    --test_size 0.2 \
-    --num_itrs 50 \
-    --lr 0.001 \
-    --num_bootstraps 3
