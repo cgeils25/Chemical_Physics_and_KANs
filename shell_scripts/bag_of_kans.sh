a02a5ef9c7fb4217d25cc955d66020180d50a1a2
Christian Geils <cgeils@email.sc.edu>
2025-04-08 12:43:21 -0400

    add shell script to run bag of kans vs single kan experiment

---
+++ shell_scripts/bag_of_kans.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+python -u full_experiments/run_bag_of_KANs_experiment.py \
+    --data_path datasets/aqueous_solubility_delaney.csv\
+    --n_trials 100 \
+    --variance_threshold 0.1 \
+    --random_seed 1738 \
+    --test_size 0.2 \
+    --num_itrs 1000 \
+    --lr 0.001 \
+    --num_bootstraps 25
