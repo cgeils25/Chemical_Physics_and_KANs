8f6d010acc6db272598c25c5889945464120e3b0
Christian Geils <cgeils@email.sc.edu>
2025-04-13 12:28:46 -0400

    50 trials and 10 bootstraps for efficiency

--- shell_scripts/bag_of_kans.sh
+++ shell_scripts/bag_of_kans.sh
@@ -2,11 +2,11 @@
 
 python -u full_experiments/run_bag_of_KANs_experiment.py \
     --data_path datasets/aqueous_solubility_delaney.csv\
-    --n_trials 100 \
+    --n_trials 50 \
     --variance_threshold 0.1 \
     --random_seed 1738 \
     --test_size 0.2 \
     --num_itrs 1000 \
     --lr 0.001 \
-    --num_bootstraps 25 \
+    --num_bootstraps 10 \
     --parallel
