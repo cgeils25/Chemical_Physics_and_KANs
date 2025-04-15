69ed56a52ac96c2440ff222924f9023d0921ef65
Christian Geils <cgeils@email.sc.edu>
2025-04-15 13:22:45 -0400

    update bag of kans experiment parameters

--- shell_scripts/bag_of_kans.sh
+++ shell_scripts/bag_of_kans.sh
@@ -8,6 +8,8 @@ python -u full_experiments/run_bag_of_KANs_experiment.py \
     --test_size 0.2 \
     --num_itrs 1000 \
     --lr 0.001 \
-    --num_bootstraps 10 \
+    --num_bootstraps 25 \
     --random_seed 1738 \
     --parallel
+
+# models are potentially underfit - best iteration is at the end. Could be worth modifying training loop to do train/val/test and pick the best for testing based on best val, then just increase the iterations 
